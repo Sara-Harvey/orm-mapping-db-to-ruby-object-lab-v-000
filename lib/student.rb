6fbe0953a06c4389e33f25e124404adf4a546fe9
Sara Harvey <saraharveynyc@gmail.com>
2019-07-13 13:24:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name =  row[1]
-    new_student.grade = row[2]
-    new_student
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student #returns student object at end of creation, hard coded since this isn't the buuilt in initialize method
   end
 
   def self.all
@@ -17,14 +17,17 @@ class Student
       SELECT *
       FROM students
     SQL
- 
+
+    #returns an array of arrays, row is an array
     DB[:conn].execute(sql).map do |row|
      self.new_from_db(row)
    end
   end
 
   def self.find_by_name(name)
-      sql = <<-SQL
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
@@ -34,15 +37,6 @@ class Student
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
-  end 
-  
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
   end
   
   def self.create_table
